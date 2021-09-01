61fc5f55dc1abc5da193a1ba495dd1537dc9f88c
Dorian <dorian.heinrichs@xiamli.com>
2021-09-01 07:39:02 +0200

    Add AllocConsole call to main.cc

--- libs/python35/src/main.cc
+++ libs/python35/src/main.cc
@@ -314,6 +314,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
                 }
             }
 
+            AllocConsole();
 #if defined(INTERNAL_ENABLED)
             EnableDebugging(events);
 #endif
